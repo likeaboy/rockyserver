9a1bfb794d3277b3805798f1c22d28111a4060a6
rocky <1186853150@qq.com>
2016-03-03 11:07:57 +0800

    handle conflict

--- src/main/java/com/rocky/server/ServletResponse.java
+++ src/main/java/com/rocky/server/ServletResponse.java
@@ -1,9 +1,11 @@
+package com.rocky.server;
+
 class ServletResponse {
 
 	public ServletResponse(){
 		System.out.println("This is ServletResponse!");
 	}
-	pubilc static void print(){
+	public static void print(){
 		System.out.println("this is print method!");
 	}
 }
